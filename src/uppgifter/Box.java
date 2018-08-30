d3340bc6c2b741a858e8a9c5eeb842fc30663d95
SD20i8 <sven1338bergius@gmail.com>
2018-08-30 17:38:25 +0200

    Gjorde inte klart den men börjar på nästa

--- src/uppgifter/Box.java
+++ src/uppgifter/Box.java
@@ -1,16 +1,24 @@
 package uppgifter;
 
-public class Box <T>{
+public class Box<T> {
 	T[] objects;
+
 	public Box(T... temp) {
 		objects = temp;
 	}
-	
-	
+
 	public static boolean uppgift5(Box a, Box b) {
-		
-		
-		
+
+		if (a.objects[0] instanceof b.objects[0].getClass())
+			return true;
+
 		return false;
+	}
+
+	public static void main(String[] args) {
+		Box a = new Box(10, 10);
+		Box b = new Box(10, 20);
+
+		System.out.println(uppgift5(a, b) ? "ja" : "nej");
 	}
 }
